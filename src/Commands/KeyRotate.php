a0e0be45da2ab4f7c814bbbc62b71c6baf8ecfa9
Brian Logan <brianldj@gmail.com>
2024-03-11 16:31:59 -0700

    feat(Key Rotate): Adding `key:rotate` command that automatically rotates the app_key

---
+++ src/Commands/KeyRotate.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Teqnifii\LaravelKeyMigration\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\warning;
+use function Laravel\Prompts\spin;
+use function Laravel\Prompts\progress;
+
+class KeyRotate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'key:rotate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Rotate the app key out for a new one.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Rotating the app key...');
+        $currentKey = config('app.key');
+        $this->addToPreviousKeys($currentKey);
+        $this->call('key:generate');
+    }
+
+    protected function addToPreviousKeys($key) {
+        $env = File::get(base_path('.env'));
+        if(str_contains($env, 'APP_PREVIOUS_KEYS')) {
+            $env = preg_replace('/APP_PREVIOUS_KEYS=(.*)/', 'APP_PREVIOUS_KEYS=$1,' . $key, $env);
+        } else {
+            $env = preg_replace('/APP_KEY=(.*)/', 'APP_KEY=$1' . PHP_EOL . 'APP_PREVIOUS_KEYS=' . $key, $env);
+        }
+        File::put(base_path('.env'), $env);
+        $this->info('App key added to previous keys.');
+    }
+}
